dbd27f188657d8745bc2902c029ec081c4e5e59f
seoil <seoil@HH311-24>
2017-11-30 15:09:05 +0900

    modified on local repository

--- Hello World/src/Pushprint.java
+++ Hello World/src/Pushprint.java
@@ -4,6 +4,7 @@ public class Pushprint {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Add Java Class File Push");
+		System.out.println("Hello world ABC");
 	}
 
 }
